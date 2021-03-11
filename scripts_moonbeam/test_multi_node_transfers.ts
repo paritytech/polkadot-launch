d6b735d568f81871a1195164f403f7379aefd144
joelamouche <estienne.antoine@gmail.com>
2021-03-11 15:03:28 +0100

    remove fn call

--- scripts_moonbeam/test_multi_node_transfers.ts
+++ scripts_moonbeam/test_multi_node_transfers.ts
@@ -224,7 +224,7 @@ export default async function main() {
   //process.kill(process.pid, 'SIGINT');
   process.exit(0);
 }
-main();
+// main();
 
 // log unhandledRejection
 process.on("unhandledRejection", (error: any) => {
