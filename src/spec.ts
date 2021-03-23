fea02ae616e815c83419b524fcd2c04ceea58b89
joelamouche <estienne.antoine@gmail.com>
2021-03-23 15:20:58 +0100

    use tabs

--- src/spec.ts
+++ src/spec.ts
@@ -4,73 +4,73 @@ import { ChainSpec } from "./types";
 const fs = require("fs");
 
 function nameCase(string: string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 // Get authority keys from within chainSpec data
 function getAuthorityKeys(chainSpec: ChainSpec) {
-  // this is the most recent spec struct
-  if (
-    chainSpec.genesis.runtime.runtime_genesis_config &&
-    chainSpec.genesis.runtime.runtime_genesis_config.palletSession
-  ) {
-    return chainSpec.genesis.runtime.runtime_genesis_config.palletSession.keys;
-  }
-  // Backward compatibility
-  return chainSpec.genesis.runtime.palletSession.keys;
+	// this is the most recent spec struct
+	if (
+		chainSpec.genesis.runtime.runtime_genesis_config &&
+		chainSpec.genesis.runtime.runtime_genesis_config.palletSession
+	) {
+		return chainSpec.genesis.runtime.runtime_genesis_config.palletSession.keys;
+	}
+	// Backward compatibility
+	return chainSpec.genesis.runtime.palletSession.keys;
 }
 
 // Remove all existing keys from `session.keys`
 export function clearAuthorities(spec: string) {
-  let rawdata = fs.readFileSync(spec);
-  let chainSpec;
-  try {
-    chainSpec = JSON.parse(rawdata);
-  } catch {
-    console.error("failed to parse the chain spec");
-    process.exit(1);
-  }
+	let rawdata = fs.readFileSync(spec);
+	let chainSpec;
+	try {
+		chainSpec = JSON.parse(rawdata);
+	} catch {
+		console.error("failed to parse the chain spec");
+		process.exit(1);
+	}
 
-  let keys = getAuthorityKeys(chainSpec);
-  keys.length = 0;
+	let keys = getAuthorityKeys(chainSpec);
+	keys.length = 0;
 
-  let data = JSON.stringify(chainSpec, null, 2);
-  fs.writeFileSync(spec, data);
-  console.log(`Starting with a fresh authority set:`);
+	let data = JSON.stringify(chainSpec, null, 2);
+	fs.writeFileSync(spec, data);
+	console.log(`Starting with a fresh authority set:`);
 }
 
 // Add additional authorities to chain spec in `session.keys`
 export async function addAuthority(spec: string, name: string) {
-  await cryptoWaitReady();
+	await cryptoWaitReady();
 
-  const sr_keyring = new Keyring({ type: "sr25519" });
-  const sr_account = sr_keyring.createFromUri(`//${nameCase(name)}`);
-  const sr_stash = sr_keyring.createFromUri(`//${nameCase(name)}//stash`);
+	const sr_keyring = new Keyring({ type: "sr25519" });
+	const sr_account = sr_keyring.createFromUri(`//${nameCase(name)}`);
+	const sr_stash = sr_keyring.createFromUri(`//${nameCase(name)}//stash`);
 
-  const ed_keyring = new Keyring({ type: "ed25519" });
-  const ed_account = ed_keyring.createFromUri(`//${nameCase(name)}`);
+	const ed_keyring = new Keyring({ type: "ed25519" });
+	const ed_account = ed_keyring.createFromUri(`//${nameCase(name)}`);
 
-  let key = [
-    sr_stash.address,
-    sr_stash.address,
-    {
-      grandpa: ed_account.address,
-      babe: sr_account.address,
-      im_online: sr_account.address,
-      parachain_validator: sr_account.address,
-      authority_discovery: sr_account.address,
-      para_validator: sr_account.address,
-      para_assignment: sr_account.address,
-    },
-  ];
+	let key = [
+		sr_stash.address,
+		sr_stash.address,
+		{
+			grandpa: ed_account.address,
+			babe: sr_account.address,
+			im_online: sr_account.address,
+			parachain_validator: sr_account.address,
+			authority_discovery: sr_account.address,
+			para_validator: sr_account.address,
+			para_assignment: sr_account.address,
+		},
+	];
 
-  let rawdata = fs.readFileSync(spec);
-  let chainSpec = JSON.parse(rawdata);
+	let rawdata = fs.readFileSync(spec);
+	let chainSpec = JSON.parse(rawdata);
 
-  let keys = getAuthorityKeys(chainSpec);
-  keys.push(key);
+	let keys = getAuthorityKeys(chainSpec);
+	keys.push(key);
 
-  let data = JSON.stringify(chainSpec, null, 2);
-  fs.writeFileSync(spec, data);
-  console.log(`Added Authority ${name}`);
+	let data = JSON.stringify(chainSpec, null, 2);
+	fs.writeFileSync(spec, data);
+	console.log(`Added Authority ${name}`);
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,61 +1,61 @@
 export interface LaunchConfig {
-  relaychain: RelayChainConfig;
-  parachains: ParachainConfig[];
-  simpleParachains: SimpleParachainConfig[];
-  hrmpChannels: HrmpChannelsConfig[];
-  types: any;
+	relaychain: RelayChainConfig;
+	parachains: ParachainConfig[];
+	simpleParachains: SimpleParachainConfig[];
+	hrmpChannels: HrmpChannelsConfig[];
+	types: any;
 }
 export interface ParachainConfig {
-  bin: string;
-  id: string;
-  rpcPort: number;
-  wsPort: number;
-  port: number;
-  balance: string;
-  flags: string[];
-  chain?: string;
+	bin: string;
+	id: string;
+	rpcPort: number;
+	wsPort: number;
+	port: number;
+	balance: string;
+	flags: string[];
+	chain?: string;
 }
 export interface SimpleParachainConfig {
-  bin: string;
-  id: string;
-  port: string;
-  balance: string;
+	bin: string;
+	id: string;
+	port: string;
+	balance: string;
 }
 export interface HrmpChannelsConfig {
-  sender: number;
-  recipient: number;
-  maxCapacity: number;
-  maxMessageSize: number;
+	sender: number;
+	recipient: number;
+	maxCapacity: number;
+	maxMessageSize: number;
 }
 export interface RelayChainConfig {
-  bin: string;
-  chain: string;
-  nodes: {
-    name: string;
-    wsPort: number;
-    port: number;
-    flags?: string[];
-  }[];
+	bin: string;
+	chain: string;
+	nodes: {
+		name: string;
+		wsPort: number;
+		port: number;
+		flags?: string[];
+	}[];
 }
 
 export interface ChainSpec {
-  name: string;
-  id: string;
-  chainType: string;
-  bootNodes: string[];
-  telemetryEndpoints: null;
-  protocolId: string;
-  properties: null;
-  forkBlocks: null;
-  badBlocks: null;
-  consensusEngine: null;
-  lightSyncState: null;
-  genesis: {
-    runtime: any; // this can change depending on the versions
-    raw: {
-      top: {
-        [key: string]: string;
-      };
-    };
-  };
+	name: string;
+	id: string;
+	chainType: string;
+	bootNodes: string[];
+	telemetryEndpoints: null;
+	protocolId: string;
+	properties: null;
+	forkBlocks: null;
+	badBlocks: null;
+	consensusEngine: null;
+	lightSyncState: null;
+	genesis: {
+		runtime: any; // this can change depending on the versions
+		raw: {
+			top: {
+				[key: string]: string;
+			};
+		};
+	};
 }
