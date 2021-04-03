5fed371ee5c332090cd3bae00560b3b9a0073704
André Silva <andrerfosilva@gmail.com>
2021-04-03 10:54:15 +0100

    update shell.nix with dependencies to build blockade

--- shell.nix
+++ shell.nix
@@ -1,9 +1,24 @@
 { pkgs ? import <nixpkgs> { } }:
-let
-  polkadot-launch = pkgs.callPackage ./default.nix { };
-in
-pkgs.mkShell {
+
+with pkgs; mkShell {
   buildInputs = [
-    polkadot-launch
+    nodejs
+    python27
+    python27Packages.pip
+    python27Packages.setuptools
+    stdenv
+    yarn
+    yarn2nix
   ];
+
+  shellHook = ''
+    # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+    # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+    export PIP_PREFIX=${toString ./.}/_build/pip_packages
+    export PYTHONPATH="$PIP_PREFIX/${python27.sitePackages}:$PYTHONPATH"
+    export PATH="$PIP_PREFIX/bin:$PATH"
+    unset SOURCE_DATE_EPOCH
+    # NOTE: the line below is commented as this is not compatible with lorri
+    # pip install blockade
+  '';
 }
