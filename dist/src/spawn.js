bebaeec2075e07d40b9f2a4fa5de81d1f0dc24ec
joelamouche <estienne.antoine@gmail.com>
2021-04-06 13:24:22 +0000

    compiled

--- dist/src/spawn.js
+++ dist/src/spawn.js
@@ -165,11 +165,11 @@ function startCollator(bin, id, wsPort, port, chain, spec, flags) {
             console.log(`Added ${flags_collator} to collator`);
         }
         p[wsPort] = child_process_1.spawn(bin, args);
-        let log = fs_1.default.createWriteStream(`${id}.log`);
+        let log = fs_1.default.createWriteStream(`${wsPort}.log`);
         p[wsPort].stdout.pipe(log);
         p[wsPort].stderr.on("data", function (chunk) {
             let message = chunk.toString();
-            if (message.substring(21, 50) === "Listening for new connections") {
+            if (message.substring(33, 62) === "Listening for new connections") {
                 resolve();
             }
             log.write(message);
