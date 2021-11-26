7d96f3290dd269e3361fb034a7e33f8a18f2f0bf
Wilfried Kopp <wilfried@parity.io>
2021-11-26 18:29:12 +0100

    Add .prettierignore

--- config.js
+++ config.js
@@ -2,91 +2,91 @@
 var flags = ["--force-authoring", "--", "--execution=wasm"];
 
 var config = {
-	"relaychain": {
-		"bin": "./bin/polkadot",
-		"chain": "rococo-local",
-		"nodes": [
+	relaychain: {
+		bin: "./bin/polkadot",
+		chain: "rococo-local",
+		nodes: [
 			{
-				"name": "alice",
-				"wsPort": 9944,
-				"port": 30444
+				name: "alice",
+				wsPort: 9944,
+				port: 30444,
 			},
 			{
-				"name": "bob",
-				"wsPort": 9955,
-				"port": 30555
+				name: "bob",
+				wsPort: 9955,
+				port: 30555,
 			},
 			{
-				"name": "charlie",
-				"wsPort": 9966,
-				"port": 30666
+				name: "charlie",
+				wsPort: 9966,
+				port: 30666,
 			},
 			{
-				"name": "dave",
-				"wsPort": 9977,
-				"port": 30777
-			}
+				name: "dave",
+				wsPort: 9977,
+				port: 30777,
+			},
 		],
-		"genesis": {
-			"runtime": {
-				"runtime_genesis_config": {
-					"configuration": {
-						"config": {
-							"validation_upgrade_frequency": 1,
-							"validation_upgrade_delay": 1
-						}
-					}
-				}
-			}
-		}
+		genesis: {
+			runtime: {
+				runtime_genesis_config: {
+					configuration: {
+						config: {
+							validation_upgrade_frequency: 1,
+							validation_upgrade_delay: 1,
+						},
+					},
+				},
+			},
+		},
 	},
-	"parachains": [
+	parachains: [
 		{
-			"bin": "./bin/polkadot-collator",
-			"id": "200",
-			"balance": "1000000000000000000000",
-			"nodes": [
+			bin: "./bin/polkadot-collator",
+			id: "200",
+			balance: "1000000000000000000000",
+			nodes: [
 				{
-					"wsPort": 9988,
-					"port": 31200,
-					"name": "alice",
+					wsPort: 9988,
+					port: 31200,
+					name: "alice",
 					flags,
-				}
-			]
+				},
+			],
 		},
 		{
-			"bin": "./bin/polkadot-collator",
-			"id": "300",
-			"balance": "1000000000000000000000",
-			"nodes": [
+			bin: "./bin/polkadot-collator",
+			id: "300",
+			balance: "1000000000000000000000",
+			nodes: [
 				{
-					"wsPort": 9999,
-					"port": 31300,
-					"name": "alice",
+					wsPort: 9999,
+					port: 31300,
+					name: "alice",
 					flags,
-				}
-			]
-		}
+				},
+			],
+		},
 	],
-	"simpleParachains": [
+	simpleParachains: [
 		{
-			"bin": "./bin/adder-collator",
-			"id": "400",
-			"port": "31400",
-			"name": "alice",
-			"balance": "1000000000000000000000"
-		}
+			bin: "./bin/adder-collator",
+			id: "400",
+			port: "31400",
+			name: "alice",
+			balance: "1000000000000000000000",
+		},
 	],
-	"hrmpChannels": [
+	hrmpChannels: [
 		{
-			"sender": 200,
-			"recipient": 300,
-			"maxCapacity": 8,
-			"maxMessageSize": 512
-		}
+			sender: 200,
+			recipient: 300,
+			maxCapacity: 8,
+			maxMessageSize: 512,
+		},
 	],
-	"types": {},
-	"finalization": false
-}
+	types: {},
+	finalization: false,
+};
 
 module.exports = config;
